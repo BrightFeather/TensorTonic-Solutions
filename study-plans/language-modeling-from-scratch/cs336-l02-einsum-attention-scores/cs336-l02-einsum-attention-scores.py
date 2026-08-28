ff7181a23fe15c0be25e8c8080c2817014af1d02
Weijia Chen <chensir1994@gmail.com>
2026-08-28 11:18:14 -0400

    Solve cs336-l02-einsum-attention-scores

---
+++ study-plans/language-modeling-from-scratch/cs336-l02-einsum-attention-scores/cs336-l02-einsum-attention-scores.py
@@ -0,0 +1,14 @@
+import torch
+import math
+
+def attention_scores(q, k, num_heads):
+    """
+    Returns: tensor of shape (batch, heads, query_length, key_length)
+    """
+    batch, query_length, d_model = q.shape
+    key_length = k.shape[1]
+    d_h = d_model // num_heads
+    
+    q = q.reshape(batch, query_length, num_heads, d_h).transpose(1,2)
+    k = k.reshape(batch, key_length, num_heads, d_h).transpose(1,2)
+    return q @ k.transpose(-2,-1) / math.sqrt(d_h)
