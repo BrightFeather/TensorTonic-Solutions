23aeceba5061f26f30fea959421e0c9178c0126c
Weijia Chen <chensir1994@gmail.com>
2026-08-31 21:56:11 -0400

    Solve cs336-l02-einsum-attention-scores

--- study-plans/language-modeling-from-scratch/cs336-l02-einsum-attention-scores/cs336-l02-einsum-attention-scores.py
+++ study-plans/language-modeling-from-scratch/cs336-l02-einsum-attention-scores/cs336-l02-einsum-attention-scores.py
@@ -9,6 +9,10 @@ def attention_scores(q, k, num_heads):
     key_length = k.shape[1]
     d_h = d_model // num_heads
     
-    q = q.reshape(batch, query_length, num_heads, d_h).transpose(1,2)
-    k = k.reshape(batch, key_length, num_heads, d_h).transpose(1,2)
-    return q @ k.transpose(-2,-1) / math.sqrt(d_h)
+    # q = q.reshape(batch, query_length, num_heads, d_h).transpose(1,2)
+    # k = k.reshape(batch, key_length, num_heads, d_h).transpose(1,2)
+    # return q @ k.transpose(-2,-1) / math.sqrt(d_h)
+    
+    q = q.reshape(batch, query_length, num_heads, d_h)
+    k = k.reshape(batch, key_length, num_heads, d_h)
+    return torch.einsum('bqhd,bkhd->bhqk',q,k) / math.sqrt(d_h)
